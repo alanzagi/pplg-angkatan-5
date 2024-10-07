6941b1f477911271aa47d25657530030b90577ee
Alan <alanzagi2007@gmail.com>
2024-10-07 22:13:43 +0700

    adding footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -546,9 +546,93 @@
         </section>
     </main>
 
-
-
-
+    <footer class="bg-transparent relative z-10 border-t border-white">
+        <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <div class="md:flex md:justify-between">
+                <div class="mb-6 md:mb-0">
+                    <a href="/" class="flex items-center">
+                        <img src="{{ asset('images/pplglogo.png') }}"
+                            class="h-8 me-3 rounded-full drop-shadow-[0_0_5px_rgba(255,255,255,1)]" alt="Alan Logo" />
+                        <span
+                            class="self-center text-2xl font-semibold whitespace-nowrap text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)]">XI
+                            PPLG</span>
+                    </a>
+                </div>
+                <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    <div>
+                        <h2
+                            class="mb-6 text-sm font-semibold text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)] uppercase">
+                            Created By</h2>
+                        <ul class="text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)] font-medium">
+                            <li class="mb-4">
+                                <a href="https://flowbite.com/" class="hover:underline">Alan "Zagi"</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2
+                            class="mb-6 text-sm font-semibold uppercase text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)]">
+                            Follow us</h2>
+                        <ul class="text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)] font-medium">
+                            <li class="mb-4">
+                                <a href="https://github.com/alanzagi" class="hover:underline">Github</a>
+                            </li>
+                            <li>
+                                <a href="https://instagram.com/famousclass_xi_pplg"
+                                    class="hover:underline">Instagram</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <span class="text-sm text-white sm:text-center">© 2024 <a
+                        href="https:/instagram.com/famousclass_xi_pplg" target="_blank"
+                        class="text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)] hover:underline">Kelas
+                        XI PPLG 5</a> | Dikelola oleh <a href="https://instagram.com/alanzagi" target="_blank"
+                        class="hover:underline text-white drop-shadow-[0_0_10px_rgba(255,255,255,1)]">Alan
+                        "Zagi"</a></span>
+                <div class="flex mt-4 sm:justify-center sm:mt-0">
+                    <a href="#" class="text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)]">
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 8 19">
+                            <path fill-rule="evenodd"
+                                d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="sr-only">Facebook page</span>
+                    </a>
+                    <a href="#" class="text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)] ms-5">
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 21 16">
+                            <path
+                                d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
+                        </svg>
+                        <span class="sr-only">Discord community</span>
+                    </a>
+                    <a href="#" class="text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)] ms-5">
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 20 17">
+                            <path fill-rule="evenodd"
+                                d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="sr-only">Twitter page</span>
+                    </a>
+                    <a href="#" class="text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,1)] ms-5">
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd"
+                                d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.449c-.902-.62.069-.607.069-.607a2.078 2.078 0 0 1 1.514 1.017 2.091 2.091 0 0 0 2.87.817 2.085 2.085 0 0 1 .622-1.313c-2.2-.25-4.513-1.101-4.513-4.902a3.84 3.84 0 0 1 1.025-2.661A3.573 3.573 0 0 1 5.128 3s.831-.266 2.726 1.014a9.324 9.324 0 0 1 4.969 0C15.719 2.734 16.55 3 16.55 3a3.564 3.564 0 0 1 .102 2.614 3.837 3.837 0 0 1 1.023 2.661c0 3.81-2.319 4.647-4.525 4.892a2.353 2.353 0 0 1 .674 1.827c0 1.32-.012 2.381-.012 2.707 0 .267.179.576.684.478A10.014 10.014 0 0 0 10 .333Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="sr-only">GitHub account</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 
     {{-- Flowbite CDN --}}
